804b92bc7f46990f9e277dfc286f8bdab99fc0ca
PatbunNF <pat_yupipat@hotmail.com>
2016-06-06 14:15:09 +0700

    Start app

---
+++ app/src/main/java/com/example/lap324_08/itpbru/itpbru.java
@@ -0,0 +1,13 @@
+package com.example.lap324_08.itpbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class itpbru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itpbru);
+    }
+}
