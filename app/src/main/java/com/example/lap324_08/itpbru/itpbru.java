cf4ff02c7299c19319c84864d4919874b87eb065
PatbunNF <pat_yupipat@hotmail.com>
2016-06-06 16:27:26 +0700

    Designing sign-up activity

--- app/src/main/java/com/example/lap324_08/itpbru/itpbru.java
+++ app/src/main/java/com/example/lap324_08/itpbru/itpbru.java
@@ -1,7 +1,9 @@
 package com.example.lap324_08.itpbru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class itpbru extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class itpbru extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itpbru);
+    }   // Main method...
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(itpbru.this, signUpActivity.class));
     }
-}
+
+}   // Main class...
