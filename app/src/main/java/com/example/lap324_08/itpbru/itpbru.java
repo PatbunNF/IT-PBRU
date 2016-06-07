0d1c1743e4d87464eedfab396b06cbae1238f110
PatbunNF <pat_yupipat@hotmail.com>
2016-06-07 15:51:53 +0700

    Finished to add value...

--- app/src/main/java/com/example/lap324_08/itpbru/itpbru.java
+++ app/src/main/java/com/example/lap324_08/itpbru/itpbru.java
@@ -7,10 +7,19 @@ import android.view.View;
 
 public class itpbru extends AppCompatActivity {
 
+    //  Explicit...
+    private MyManager myManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itpbru);
+
+        myManager = new MyManager(this);
+
+        //  Test to add new user...
+        //myManager.addNewUser("123", "Name", "Sur", "User", "Pass");
+
     }   // Main method...
 
     public void clickSignUpMain(View view) {
