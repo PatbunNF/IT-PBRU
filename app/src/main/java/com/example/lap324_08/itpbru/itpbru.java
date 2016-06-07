e15775ab6719badcf80485dce2782fc6e667f3fe
PatbunNF <pat_yupipat@hotmail.com>
2016-06-07 15:59:13 +0700

    Finished testing to add/delete value to database...

--- app/src/main/java/com/example/lap324_08/itpbru/itpbru.java
+++ app/src/main/java/com/example/lap324_08/itpbru/itpbru.java
@@ -1,6 +1,7 @@
 package com.example.lap324_08.itpbru;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,15 @@ public class itpbru extends AppCompatActivity {
         //  Test to add new user...
         //myManager.addNewUser("123", "Name", "Sur", "User", "Pass");
 
+        //  Delete all sqlite...
+        deleteAllSQLite();
+
     }   // Main method...
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name, MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManager.user_table, null, null);
+    }   //  Delete
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(itpbru.this, signUpActivity.class));
