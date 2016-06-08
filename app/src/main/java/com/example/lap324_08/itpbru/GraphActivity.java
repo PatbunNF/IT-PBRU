5a044274e862773c299c7c6fe62e54d07b337cf8
PatbunNF <pat_yupipat@hotmail.com>
2016-06-08 16:07:48 +0700

    Finished uploading receiving/spending to Acc...

---
+++ app/src/main/java/com/example/lap324_08/itpbru/GraphActivity.java
@@ -0,0 +1,15 @@
+package com.example.lap324_08.itpbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GraphActivity extends AppCompatActivity {
+
+    //  Explicit...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }   //  Main method...
+}   //  Main class...
