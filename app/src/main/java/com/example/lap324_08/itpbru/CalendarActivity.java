8023fe60171c4fe9bdce7061bef001f06912a9e1
PatbunNF <pat_yupipat@hotmail.com>
2016-06-08 16:49:57 +0700

    Creating show-graph activity unfinished...

--- app/src/main/java/com/example/lap324_08/itpbru/CalendarActivity.java
+++ app/src/main/java/com/example/lap324_08/itpbru/CalendarActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CalendarView;
 
 public class CalendarActivity extends AppCompatActivity {
@@ -29,6 +30,14 @@ public class CalendarActivity extends AppCompatActivity {
         });
 
     }   //  Main method...
+
+    public void clickShowgraph(View view) {
+
+        Intent intent = new Intent(CalendarActivity.this, GraphActivity.class);
+        intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
+        startActivity(intent);
+
+    }
 
     private void confirmDialog(int dayOfMonth, int month, int year) {
 
--- app/src/main/java/com/example/lap324_08/itpbru/GraphActivity.java
+++ app/src/main/java/com/example/lap324_08/itpbru/GraphActivity.java
@@ -1,15 +1,56 @@
 package com.example.lap324_08.itpbru;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class GraphActivity extends AppCompatActivity {
 
     //  Explicit...
+    private String urlJSON = "http://swiftcodingthai.com/pbru2/get_account_where.php";
+    private String[] loginStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        loginStrings = getIntent().getStringArrayExtra("Login");
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("id_user", "1")
+                .build();
+
+        Request.Builder builder = new Request.Builder();
+        Request request= builder.url(urlJSON).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Log.d("June8th", "JSON ==>" + response.body());
+            }
+        });
+
     }   //  Main method...
+
+
+
 }   //  Main class...
